4f3e11c9a86c8a3e522bd950406105547bc85dea
joffreyverd <jverd@protonmail.com>
2024-09-06 16:53:33 +0200

    Feature: first commit

---
+++ app/Traits/ApiResponses.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponses {
+    protected function ok($message, $data): JsonResponse {
+        return $this->success($message, $data, 200);
+    }
+    protected function success($message, $data, $code = 200): JsonResponse {
+        return response()->json([
+            'data' => $data,
+            'message' => $message,
+            'code' => $code
+        ], $code);
+    }
+
+    protected function error($message, $code): JsonResponse {
+        return response()->json([
+            'message' => $message,
+            'code' => $code
+        ], $code);
+    }
+}
