d27518fd5f748ecd404f7a3affa9b89917851122
joffreyverd <jverd@protonmail.com>
2024-09-12 08:13:21 +0200

    Bugfix: change builder import

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use App\Http\Filters\v1\QueryFilter;
-use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
